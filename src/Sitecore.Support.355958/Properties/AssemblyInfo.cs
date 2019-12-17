3e1d8f5dfa3c0f946bae933219f92a6d2c555258
sc-AmitPatel <ampa@sitecore.net>
2019-12-17 10:23:14 -0500

    Initial Commit

---
+++ src/Sitecore.Support.355958/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.355958")]
+[assembly: AssemblyProduct("Sitecore.Support.355958")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.355958/SelectRenderingDatasourceForm.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Shell.Applications.Dialogs;
+using Sitecore.Shell.Applications.Dialogs.ItemLister;
+using Sitecore.Web.UI.HtmlControls;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+using Sitecore.XA.Foundation.SitecoreExtensions.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Foundation.LocalDatasources.Dialogs
+{
+    public class SelectRenderingDatasourceForm : Sitecore.XA.Foundation.LocalDatasources.Dialogs.SelectRenderingDatasourceForm
+    {
+        protected new void OnLoad(EventArgs e)
+        {
+            Assert.ArgumentNotNull(e, "e");
+            base.OnLoad(e);
+            if (!Context.ClientPage.IsEvent)
+            {                
+                ((DatasourceMultiRootTreeview)base.Treeview).ShowNodeExtendedCommands = true;
+            }
+        }
+    }
+}
